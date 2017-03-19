d6e1069c3eba492a7f794e85ba0bc3bb936897d9
Emiel Koning <Emiel.Koning@gmail.com>
2017-03-19 14:19:07 +0100

    aanpassing

--- char_lcd.py
+++ char_lcd.py
@@ -1,46 +1,23 @@
 #!/usr/bin/python
 # Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
-
-print("start..." )
-
 import Adafruit_CharLCD as LCD
 
-print("import klaar" )
-
-
-
 # Raspberry Pi pin configuration:
-lcd_rs        = 25  # Note this might need to be changed to 21 for older revision Pi's.
-lcd_en        = 24
-lcd_d4        = 23
-lcd_d5        = 17
-lcd_d6        = 21
-lcd_d7        = 22
+lcd_rs = 25  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 21
+lcd_d7 = 22
 lcd_backlight = 4
-
-# BeagleBone Black configuration:
-# lcd_rs        = 'P8_8'
-# lcd_en        = 'P8_10'
-# lcd_d4        = 'P8_18'
-# lcd_d5        = 'P8_16'
-# lcd_d6        = 'P8_14'
-# lcd_d7        = 'P8_12'
-# lcd_backlight = 'P8_7'
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows    = 2
 
-# Alternatively specify a 20x4 LCD.
-# lcd_columns = 20
-# lcd_rows    = 4
-
 # Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
-
-print("Na init lcd")
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 # Print a two line message
 lcd.message('Hello\nworld!')
@@ -89,3 +66,9 @@ lcd.clear()
 lcd.message('Goodbye!')
 # Turn backlight on.
 lcd.set_backlight(1)
+
+while True:
+    text = raw_input("Voer wat in")
+    Lcd.clear()
+    lcd.message(text)
+    time.sleep(2)
