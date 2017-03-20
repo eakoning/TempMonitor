6947ff08c64dd92c63806cab213bc19fcab45841
Emiel Koning <Emiel.Koning@gmail.com>
2017-03-20 23:05:50 +0100

    pylint

--- switch.py
+++ switch.py
@@ -1,12 +1,12 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-  input_state = GPIO.input(18)
-  if input_state == False:
-    print("Button pressed")
-    time.sleep(0.2)
+    input_state = GPIO.input(18)
+    if not input_state:
+        print("Button pressed")
+        time.sleep(0.2)
