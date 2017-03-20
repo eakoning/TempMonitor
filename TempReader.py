c05eee15abb691cf242a75a597388e366917a066
Emiel Koning <Emiel.Koning@gmail.com>
2017-03-20 22:29:24 +0100

    pylint.rc renamed to .pylintrc for VS Code

--- TempReader.py
+++ TempReader.py
@@ -8,14 +8,15 @@ os.system('modprobe w1-therm')
 def determine_sensor_paths():
     sensor_folder_pattern = "28-00000*"
     devices_base_dir = "/sys/bus/w1/devices/"
-    sensor_path_glob = os.path.join(devices_base_dir, sensor_folder_pattern)
-    return list(map(lambda p: os.path.join(sensor_path_glob, "w1_slave")))
+    sensor_path_glob = glob.glob(os.path.join(devices_base_dir, sensor_folder_pattern))
+    return [os.path.join(p, "w1_slave") for p in sensor_path_glob]
 
 def get_file_contents(path):
     f = open(path, 'r')
     lines = f.readlines()
     f.close()
     return lines
+
 
 def read_temperature(path):
     lines = get_file_contents(path)
@@ -26,7 +27,7 @@ def read_temperature(path):
 
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
+        temp_string = lines[1][equals_pos + 2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
@@ -34,6 +35,6 @@ def read_temperature(path):
 sensor_paths = determine_sensor_paths()
 
 while True:
-    for path in sensor_paths:
-        print("%s %s" % path, read_temperature(path))
+    for sp in sensor_paths:
+        print("%s %s" % sp, read_temperature(sp))
         time.sleep(1)
